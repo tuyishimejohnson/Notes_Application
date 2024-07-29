181b66ca2f872a2cbbd880017f931991f6006363
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-29 04:45:53 +0200

    Created a new project of notes application using vite

---
+++ notes_application/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-yellow-500'>
+            <h1>Hello world</h1>
+      </div>
+    
+      <div className="card">
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
